f47cd038bb27fa4f174cc3e1ba8fde90fb1bbc35
peregrinogris <colmeiro@gmail.com>
2009-08-15 16:34:24 -0300

    This is the first working draft of the extension. I have yet to organize the files inside it and rename a lot of things from the first dummy extension that I used to start coding.

--- chrome/content/netlistener/netListener.js
+++ chrome/content/netlistener/netListener.js
@@ -3,6 +3,10 @@ FBL.ns(function() { with (FBL) {
 const Cc = Components.classes;
 const Ci = Components.interfaces;
 
+var filtersActive = false;
+var modifiedRows = [];
+var currentFilter = /google/;
+
 // Module implementation
 Firebug.NetListenerModule = extend(Firebug.Module,
 {
@@ -21,6 +25,24 @@ Firebug.NetListenerModule = extend(Firebug.Module,
 
         // Unregister NetMonitor listener
         Firebug.NetMonitor.NetRequestTable.removeListener(this.netListener);
+    },
+
+    onToggleAdvFilters: function()
+    {
+        filtersActive = !filtersActive;
+
+        if(!filtersActive)
+        {
+           removeFilter(modifiedRows);
+        }
+        else
+        {
+            var newFilter = prompt("Input desired filter RegExp. It'll be tested against the request's URL", currentFilter.source);
+            if(newFilter)
+                currentFilter = new RegExp(newFilter);
+
+            applyFilter(modifiedRows, currentFilter);
+        }
     }
 });
 
@@ -32,15 +54,43 @@ NetListener.prototype =
     onCreateRequestEntry: function(panel, row)
     {
         // Access file object that contains all info about the request
-        FBTrace.sysout("netPanelListener; " + row.repObject.href);
+        // FBTrace.sysout("netPanelListener; " + row.repObject.href);
 
         // Customize UI.
-        if(/google/.test(row.repObject.href))
-            row.setAttribute("style", "background-color: orange; font-size: 12px; color: red");
-        else
-            row.setAttribute("style", "font-size: 8px;");
+        if(filtersActive)
+        {
+            applyFilter([row],currentFilter);
+            //I save a refference to the modified row so I can enable or disable the styles later.
+            modifiedRows.push(row);
+        }
     }
 };
+
+//********************************************
+//Helper Functions
+function applyFilter(rows,regexp)
+{
+    var style ="", row;
+    for(var i = 0; i < rows.length; i++)
+    {
+        row = rows[i];
+        if(regexp.test(row.repObject.href))
+            style = "background-color: orange; font-size: 12px; color: red";
+        else
+            style = "font-size: 8px;";
+
+        row.setAttribute("style", style);
+    }
+};
+
+function removeFilter(rows)
+{
+    for(var i = 0; i < rows.length; i++)
+    {
+        rows[i].setAttribute("style", "");
+    }
+};
+
 
 // Registration
 Firebug.registerModule(Firebug.NetListenerModule);
